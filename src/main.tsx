221f959f7262907b11ca972640134d0bdfd06420
Mustafa ismail <mustafaismail22@users.noreply.github.com>
2022-03-19 14:13:03 +0200

    🍗

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-white dark:bg-zinc-900">
+      <h1 className="text-3xl font-bold text-black dark:text-white">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{html,tsx}", "./index.html"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
