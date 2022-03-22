06a1bfefd93169d5e1739ddafc71579c740db710
Mustafa ismail <mustafaismail22@users.noreply.github.com>
2022-03-22 17:33:38 +0200

    Hello World!

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import "./index.css";
 
+ReactGA.initialize("UA-43653001-4");
+
 function App() {
+  React.useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-white dark:bg-zinc-900">
-      <h1 className="text-3xl font-bold text-black dark:text-white">
-        Hello world!
+    <div className="flex overflow-hidden justify-center items-center h-screen w-screen bg-white dark:bg-black">
+      <h1
+        data-text="Hello World!"
+        className="glitch font-bold dark:text-white bg-inherit text-[12vw] sm:text-[10vw] "
+      >
+        Hello World!
       </h1>
+      <div className="scanlines"></div>
     </div>
   );
 }
